e94b6e4fd0a158dcb97ea6615c447f4e0bb08f62
Rhiskey <first3395@gmail.com>
2019-11-08 17:48:41 +0300

    Добавил построение в пространстве кривых

--- ForcesMomentsToFunc/getForceOnRPMandFlow.m
+++ ForcesMomentsToFunc/getForceOnRPMandFlow.m
@@ -1,4 +1,4 @@
-function [ForceOrMoment] = getForceOnRPMandFlow(RPM_comb, plotdata, RPM_find)
+function [ForceOrMoment] = getForceOnRPMandFlow(RPM_comb, plotdata, RPM_find, flag)
 
 
 %% Fit: 
@@ -14,16 +14,30 @@ fitresult = fit( xData, yData, ft, 'Normalize', 'on' );
 index=find(xData==RPM_find);
 Y_point = feval(fitresult,RPM_find);
 
-hold on
-%See graphically
-h = plot( fitresult, xData, yData );
-h1 = plot(fitresult, RPM_find, Y_point, 'o');
-legend( 'Исходные частоты', 'Кубическая интерполяция','Заданная частота','Кубическая интерполяция', 'Location', 'best', 'Interpreter', 'none' );
-xlabel( 'RPM', 'Interpreter', 'none' );
-ylabel( 'Force or Moment', 'Interpreter', 'none' );
-grid on
-hold off
+
+if flag ~= 0
+    %See graphically
+    hold on
+
+    h = plot( fitresult, xData, yData );
+    h1 = plot(fitresult, RPM_find, Y_point, 'o');
+    legend( 'Исходные частоты', 'Кубическая интерполяция','Заданная частота','Кубическая интерполяция', 'Location', 'best', 'Interpreter', 'none' );
+    % plot(fitresult,X_point, Y_point, 'o');
+    % curvefit(RPM_find);
+    xlabel( 'RPM', 'Interpreter', 'none' );
+    if flag == 1
+    %     figure('Name','Зависимость силы от частоты на заданном расходе')
+        ylabel( 'Force, N', 'Interpreter', 'none' );
+    elseif flag == 2
+    %     figure('Name','Зависимость момента от частоты на заданном расходе')
+        ylabel( 'Moment, N*m', 'Interpreter', 'none' );  
+    else
+    ylabel( 'Force/Moment', 'Interpreter', 'none' );  
+    end
+    grid on
+    hold off
+end
+
 
 ForceOrMoment = Y_point;
-
 
